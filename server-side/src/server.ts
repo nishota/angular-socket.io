eee9629e5678b57599b372439d284bb5cc852362
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-10-27 21:56:37 +0900

    canvasに文字を描画させる

--- server-side/src/server.ts
+++ server-side/src/server.ts
@@ -2,6 +2,8 @@ import Express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
 
+import { Canvas } from 'canvas';
+
 const app: Express.Express = Express();
 const server: http.Server = http.createServer(app);
 const io: socketio.Server = socketio(server);
@@ -13,11 +15,20 @@ io.on('connection', (socket: socketio.Socket) => {
     const conect = query.conect;
     console.log(conect);
 
+    const ctx: CanvasRenderingContext2D = new Canvas(200, 200).getContext('2d');
+
     socket.on('disconnect', (data: any) => {
     });
+
     socket.on('message', (data: { text: string, name: string }) => {
         console.log(`${data.name} : ${data.text}`);
+
+        ctx.font = '30px';
+        ctx.rotate(0.1);
+        ctx.fillText(data.text, 50, 100);
+
         io.emit('sync-data', { name: data.name, text: data.text, date: new Date() });
+        io.emit('sync-canvas', { font: '30px Impact', rotate: 0.1, text: {data: data.text, height: 50, width: 100} });
     });
 });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit, ElementRef,  } from '@angular/core';
 import { WebSocketService } from './web-socket.service';
 import { FormControl } from '@angular/forms';
-import { stringify } from 'querystring';
 
 @Component({
   providers: [WebSocketService],
@@ -9,11 +8,14 @@ import { stringify } from 'querystring';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit{
   connection;
   text: FormControl;
   name: FormControl;
-  chat: { text: string, date: Date}[];
+  chat: { text: string, date: Date }[];
+
+  @ViewChild('myCanvas', { static: true }) myCanvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
 
   constructor(private webSocketService: WebSocketService) {
     this.text = new FormControl('');
@@ -24,19 +26,31 @@ export class AppComponent implements OnInit, OnDestroy {
   onClick() {
     this.webSocketService.emit(
       'message',
-      {text: this.text.value, name: this.name.value}
-      );
+      { text: this.text.value, name: this.name.value }
+    );
     this.text.setValue('');
   }
 
   ngOnInit() {
     this.webSocketService.connect('conect=Hello,ServerSide!');
-    this.connection = this.webSocketService.on('sync-data').subscribe((data: {name: string, text: string, date: Date}) => {
-      this.chat.unshift(data);
-    });
+    this.ctx = this.myCanvas.nativeElement.getContext('2d');
   }
 
   ngOnDestroy() {
     this.connection.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.connection = this.webSocketService.on('sync-data')
+    .subscribe((data: { name: string, text: string, date: Date, context: any }) => {
+      this.chat.unshift(data);
+    });
+    this.connection = this.webSocketService.on('sync-canvas')
+    .subscribe((data: {font: string, rotate: number, text: {data: string, height: number, width: number} }) => {
+      this.ctx.font = data.font;
+      this.ctx.rotate(data.rotate);
+      this.ctx.fillText(data.text.data, data.text.height, data.text.width);
+      this.ctx.restore();
+    });
   }
 }
